c4b49039e001f2570b219c787db565e366468204
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-08-27 16:21:53 +0700

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="text-3xl text-green-500 font-bold underline">
+      Hello world!
+    </h1>
       </header>
     </div>
   );
